bbc790e87a2a38aa1da9c1750eaabd5969e7052a
toobaditstaken <alexabbott10@yahoo.com>
2019-04-05 04:45:14 -0500

    added starting variables in javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+var number = 30;
+var intervalId;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+var questions = [{
+    question:
+    answerList:
+    answer: 0
+}]
