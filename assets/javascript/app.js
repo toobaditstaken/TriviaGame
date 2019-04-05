feeff8ea0caef614ef2b0f0c8e03ae1b90d461b9
toobaditstaken <alexabbott10@yahoo.com>
2019-04-05 05:17:16 -0500

    added in questions and answers for quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,31 @@ var incorrectAnswers = 0;
 var unanswered = 0;
 
 var questions = [{
-    question:
-    answerList:
+    question: "What year did the events of the first Bioshock game happen?",
+    answerList: ["1960","1968","1957","1962"],
     answer: 0
-}]
+},{
+    question: "What is the name of the Little Sister protector?",
+    answerList: ["Low Diver", "Delta Series", "Big Daddy", "Leather Golem"],
+    answer: 2
+},{
+    question: "Splicer's got their name because of their addiction to?",
+    answerList: ["Plasmids", "Adam", "Eve", "Movies"],
+    answer: 1
+},{
+    question: "Who is the founder of Rapture?",
+    answerList: ["Julie Langford", "Handsome Jack", "Andrew Ryan", "Frank Fontaine"],
+    answer: 2
+},{
+    question: "What was the first Plasmid you received?",
+    answerList: ["Electro Bolt", "Incinerate", "Insect Swarm", "Winter Blast"],
+    answer: 0
+},{
+    question: "Bioshock is greatly infuenced by what philosophy?",
+    answerList: ["Taoism", "Marxism", "Nihilism" ,"Objectivism"],
+    answer: 3
+},{
+    question: "Rapture is a city located?",
+    answerList: ["Under the sea", "In the sky", "Deep underground", "In space"],
+    answer: 0
+}];
